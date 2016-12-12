e9bb6cd25e623af20b52f68daf2eec47d7e0d696
Manish <manish_shah@intuit.com>
2016-12-12 09:28:36 +0530

    add print in init.

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -39,6 +39,7 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("init is running " + function)
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
